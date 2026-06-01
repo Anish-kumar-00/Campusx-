9d552e467f8daa2c04a197bb074baefc46f4dc0d
Anish Kumar <ay6704197@gmail.com>
2026-06-01 08:40:58 +0530

    update 27-3.py

--- Python/Set/27-3.py
+++ Python/Set/27-3.py
@@ -1,4 +1,4 @@
-2.0.1 Q3: Write a program to Check if a given string is binary string of or not.
+"""2.0.1 Q3: Write a program to Check if a given string is binary string of or not.
 A string is said to be binary if it’s consists of only two unique characters.
 Take string input from user.
 Input: str = "01010101010"
@@ -7,11 +7,11 @@ Input: str = "1222211"
 Output: Yes
 Input: str = "Campusx"
 Output: No
-
+"""
 str="01010101010"
 #input("Enter a string:-")
 uniue_str=set(str)
-x=len(str)
+x=len(uniue_str)
 print(x)
 if x==2:
     print("Yes Binary string")
