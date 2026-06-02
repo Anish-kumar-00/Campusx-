177f16839961c26bda3243ef226be9c634686664
Anish Kumar <ay6704197@gmail.com>
2026-06-02 10:12:26 +0530

    update 27-2.py

---
+++ Python/Set/27-2.py
@@ -0,0 +1,16 @@
+"""
+###Q2: Write a program to count unique number of 
+vowels using sets in a given string. Lowercase
+and upercase vowels will be taken as different.
+Input:
+Str1 = "hands-on data science mentorship progrAm with live classes at affordable fee only on CampusX"
+Output:
+No of unique vowels-6
+"""
+
+Str1 = set("hands-on data science mentorship progrAm with live classes at affordable fee only on CampusX")
+#print(Str1)
+A= set("aeiouAEIOU")
+p=Str1&A
+print(len(p))
+
