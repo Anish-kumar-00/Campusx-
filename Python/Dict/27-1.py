dd41695980a7f42b5ddde132b82475bfaa0e0c35
Anish Kumar <ay6704197@gmail.com>
2026-06-02 10:16:32 +0530

    update 27-1.py

--- Python/Dict/27-1.py
+++ Python/Dict/27-1.py
@@ -1,8 +1,11 @@
 3.0.1 Q1: Key with maximum unique values
-Given a dictionary with values list, extract key whose value has most unique values.
+Given a dictionary with values list, extract key whose 
+value has most unique values.
 Example 1:
 Input:
-test_dict = {"CampusX" : [5, 7, 9, 4, 0], "is" : [6, 7, 4, 3, 3], "Best" : [9, 9, 6, 5, 5]}
+test_dict = {"CampusX" : [5, 7, 9, 4, 0], "is" : 
+    [6, 7, 4, 3, 3],
+"Best" : [9, 9, 6, 5, 5]}
 Output:
 CampusX
 Example 2:
@@ -10,4 +13,3 @@ Input:
 test_dict = {"CampusX" : [5, 7, 7, 7, 7], "is" : [6, 7, 7, 7], "Best" : [9, 9, 6, 5, 5]}
 Output:
 Best
-[ ]: # write yo
