80028894d4dca5a767b4341597d16387dcbce0be
Anish Kumar <ay6704197@gmail.com>
2026-06-02 10:39:44 +0530

    update 27-1.py

--- Python/Dict/27-1.py
+++ Python/Dict/27-1.py
@@ -1,3 +1,4 @@
+"""
 3.0.1 Q1: Key with maximum unique values
 Given a dictionary with values list, extract key
 whose 
@@ -14,3 +15,9 @@ Input:
 test_dict = {"CampusX" : [5, 7, 7, 7, 7], "is" : [6, 7, 7, 7], "Best" : [9, 9, 6, 5, 5]}
 Output:
 Best
+"""
+test_dict = {"CampusX" : [5, 7, 9, 4, 0], "is" : 
+    [6, 7, 4, 3, 3],
+"Best" : [9, 9, 6, 5, 5]}
+for i in test_dict:
+     
