61c1423712bc018ffd5b3d9a18eb65651a314aa3
Anish Kumar <ay6704197@gmail.com>
2026-06-13 17:40:40 +0530

    update students.py

---
+++ Python/class and object/students.py
@@ -0,0 +1,17 @@
+#class creation in python
+
+class Student :
+    x="anish"
+# 1st Object creation same as java
+obj=Student()
+print(obj.x)
+# 2nd object creation
+print(Student().x)
+
+"""it is same as static variable in c++ but it can be change
+in order to make instance member variable we have to make
+variable inside constructor 
+
+
+
+"""
