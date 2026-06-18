32e7ccd1dc50530d57af1a798806e1b88496a6e5
Anish Kumar <ay6704197@gmail.com>
2026-06-18 17:15:50 +0530

    update session-13-numpy-fundamentals.ipynb - Colab-invert.py

--- Numpy/session-13-numpy-fundamentals.ipynb - Colab-invert.py
+++ Numpy/session-13-numpy-fundamentals.ipynb - Colab-invert.py
@@ -1,15 +1,10 @@
-l=[[j for j in range(5)],[j for j in range(5,10)]]
-print(l)
 
-m=[[0,1,2,3,4],
-   [5,6,7,8,9]
-  ]
-  
-k=[i[1:4]for i in m]
-print(k)
 
+"""Q-1 Create a null vector of size 10 but the fifth value which is 1. """
 import numpy as np
-h=np.array(m)
-print(h)
-o=h[:,1:4]
-print(o)
+l=np.array([1 if i==4 else 0 for i in range(10)  ])#if else in list comprehension
+print(l)
+"""import numpy as np
+l=np.zeros(10) 
+l[4]=1
+print(l)"""
